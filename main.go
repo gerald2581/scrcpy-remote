7aca7440848a80b404735da76507f8fdeadce3a8
gerald2581 <geraldasia77@gmail.com>
2026-06-14 04:36:37 +0700

    feat(main): wire api and embedded web, bind 127.0.0.1, open browser

--- main.go
+++ main.go
@@ -1,7 +1,58 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"net/http"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
+
+	"scrcpy-remote/internal/api"
+	"scrcpy-remote/internal/devices"
+	"scrcpy-remote/internal/tools"
+	"scrcpy-remote/web"
+)
+
+func openBrowser(url string) {
+	switch runtime.GOOS {
+	case "windows":
+		exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
+	case "darwin":
+		exec.Command("open", url).Start()
+	default:
+		exec.Command("xdg-open", url).Start()
+	}
+}
 
 func main() {
-	fmt.Println("scrcpy-remote")
+	port := flag.Int("port", devices.DefaultPort, "dashboard port (127.0.0.1)")
+	flag.Parse()
+
+	home, _ := os.UserHomeDir()
+	cfgPath := filepath.Join(home, ".scrcpy-remote", "config.json")
+
+	srv := &api.Server{
+		ConfigPath: cfgPath,
+		Runner:     tools.ExecRunner{},
+		ADBPath:    "adb",
+		ScrcpyPath: "scrcpy",
+		LaunchFn: func(args []string) error {
+			return exec.Command("scrcpy", args...).Start()
+		},
+	}
+
+	mux := http.NewServeMux()
+	mux.Handle("/api/", srv.Handler())
+	mux.Handle("/", http.FileServer(http.FS(web.FS())))
+
+	addr := fmt.Sprintf("127.0.0.1:%d", *port)
+	url := "http://" + addr
+	fmt.Println("scrcpy-remote dashboard:", url)
+	openBrowser(url)
+	if err := http.ListenAndServe(addr, mux); err != nil {
+		fmt.Fprintln(os.Stderr, "server error:", err)
+		os.Exit(1)
+	}
 }
